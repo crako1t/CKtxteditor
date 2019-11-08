312e6de66a4df4cb5f18cec28cdf619c8da94ad6
Craig Koiter <craigkoiter@gmail.com>
2019-11-08 10:35:23 -0500

    pull in characters from terminal, end program on 'q'

--- editor.c
+++ editor.c
@@ -1,2 +1,11 @@
 //CK Text Editor for Linux
 //Author: Craig Koiter
+
+#include <termios.h>
+#include <unistd.h>
+
+int main() {
+    char c;
+    while (read(STDIN_FILENO, &c, 1) ==1 && c != 'q');
+    return 0;
+}
